35cea7db561021f0f64778e4f442ad1115fce767
Carter Perkins <cartersperkins@gmail.com>
2021-05-18 22:50:28 -0700

    Added hyperparameter tuning

--- main.py
+++ main.py
@@ -7,5 +7,7 @@ from preprocessing import preprocess
 if __name__ == '__main__':
     df = preprocess()
     for mode in ['tf_idf', 'bag_of_words']:
-        results = run_experiment(df, 'naive_bayes', {'mode': mode})
+        results = run_experiment(df, 'naive_bayes',
+                                 {'mode': mode,
+                                  'param_grid': {'var_smoothing': [pow(10, -k) for k in range(20)]}})
         print(mode, results)
--- naive_bayes.py
+++ naive_bayes.py
@@ -1,58 +1,49 @@
-from typing import Dict
+from typing import Dict, Tuple
 
+import numpy as np
 import pandas as pd
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import f1_score, roc_auc_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.naive_bayes import GaussianNB
 
 from utility import split_dataframe
 
 
-def bag_of_words(df: pd.DataFrame) -> Dict:
-    train_df, test_df = train_test_split(df, test_size=0.3, random_state=0)
-    X_train, y_train = split_dataframe(train_df)
-    X_test, y_test = split_dataframe(test_df)
-
+def bag_of_words(X_train: np.ndarray, X_test: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     # Naive Bayes with Bag of Words
     cv = CountVectorizer()
     X_train_counts = cv.fit_transform(X_train).toarray()
     X_test_counts = cv.transform(X_test).toarray()
 
-    gnb = GaussianNB()
-    model = gnb.fit(X_train_counts, y_train)
-    y_pred = model.predict(X_test_counts)
+    return (X_train_counts, X_test_counts)
 
-    return {'roc_auc_score': roc_auc_score(y_test, y_pred),
-            'f1_score': f1_score(y_test, y_pred)}
-
-def tf_idf(df: pd.DataFrame) -> Dict:
-    train_df, test_df = train_test_split(df, test_size=0.3, random_state=0)
-    X_train, y_train = split_dataframe(train_df)
-    X_test, y_test = split_dataframe(test_df)
-
-    # Naive Bayes with Bag of Words
-    cv = CountVectorizer()
-    X_train_counts = cv.fit_transform(X_train).toarray()
-    X_test_counts = cv.transform(X_test).toarray()
+def tf_idf(X_train: np.ndarray, X_test: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    X_train_counts, X_test_counts = bag_of_words(X_train, X_test)
 
     # Naive Bayes with TF-IDF Vectors
     tt = TfidfTransformer()
     X_train_tfidf = tt.fit_transform(X_train_counts).toarray()
     X_test_tfidf = tt.transform(X_test_counts).toarray()
 
+    return (X_train_tfidf, X_test_tfidf)
+
+def run_naive_bayes(df: pd.DataFrame, mode: str, param_grid: Dict) -> Dict:
+    train_df, test_df = train_test_split(df, test_size=0.3, random_state=0)
+    X_train, y_train = split_dataframe(train_df)
+    X_test, y_test = split_dataframe(test_df)
+
+    callbacks = {
+        'bag_of_words': lambda: bag_of_words(X_train, X_test),
+        'tf_idf': lambda: tf_idf(X_train, X_test)}
+
+    X_train, X_test = callbacks[mode]()
     gnb = GaussianNB()
-    model = gnb.fit(X_train_tfidf, y_train)
-    y_pred = model.predict(X_test_tfidf)
+    clf = GridSearchCV(gnb, param_grid).fit(X_train, y_train)
+    print(clf)
+    y_pred = clf.predict(X_test)
 
     return {'roc_auc_score': roc_auc_score(y_test, y_pred),
             'f1_score': f1_score(y_test, y_pred)}
-
-def run_naive_bayes(df: pd.DataFrame, mode: str) -> Dict:
-    callbacks = {
-        'bag_of_words': lambda: bag_of_words(df),
-        'tf_idf': lambda: tf_idf(df)}
-
-    return callbacks[mode]()
 
